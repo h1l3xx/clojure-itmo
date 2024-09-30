c524db4f71757328fda66eb71b1251d091bca156
h1l3xx <alksmlkv@gmail.com>
2024-10-01 02:33:50 +0300

    fear(prob21): tail recur solution for problem 21 (includes test)

---
+++ src/prob21/tail_recur.clj
@@ -0,0 +1,22 @@
+(ns prob21.tail_recur)
+
+(defn sum-of-divisors [n]
+  (loop [i 1 acc 0]
+    (if (>= i n)
+      acc
+      (if (zero? (mod n i))
+        (recur (inc i) (+ acc i))
+        (recur (inc i) acc)))))
+
+(defn sum-amicable-numbers [limit]
+  (loop [n 2 acc 0]
+    (if (>= n limit)
+      acc
+      (let [pair-sum (sum-of-divisors n)]   ;; Сумма делителей n
+        (if (and (not= n pair-sum)          ;; Если n не равно pair-sum
+                 (= n (sum-of-divisors pair-sum)))  ;; И если сумма делителей pair-sum равна n
+          (recur (inc n) (+ acc n))
+          (recur (inc n) acc))))))
+
+(defn solve [n]
+  (sum-amicable-numbers n))
--- test/problems_test.clj
+++ test/problems_test.clj
@@ -1,10 +1,13 @@
 (ns problems-test
   (:require [clojure.test :refer [deftest is]]
             [prob10.modular :as problem-10-modular]
-            [prob10.lazy :as problem-10-lazy]))
+            [prob10.lazy :as problem-10-lazy]
+            [prob21.tail_recur :as problem-21-tail]))
 
 (deftest test-problem-10-modular-solve
   (is (= (problem-10-modular/solve 2000000) 142913828922)))
 (deftest test-problem-10-lazy-solve
   (is (= (problem-10-lazy/solve 2000000) 142913828922)))
+(deftest test-problem-21-tail-recur-solve
+  (is (= (problem-21-tail/solve 10000) 31626)))
 
