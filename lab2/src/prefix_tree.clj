6da017e8565a02b690fb0d9e54ae352d3e085053
h1l3xx <alksmlkv@gmail.com>
2024-10-13 17:40:26 +0300

    feat(lab2): add filter and map tasks

--- lab2/src/prefix_tree.clj
+++ lab2/src/prefix_tree.clj
@@ -1,7 +1,6 @@
 (ns prefix_tree)
 
-(defn create-node
-  []
+(defn create-node []
   {:is-end false :children {}})
 
 (defn insert-word [trie word]
@@ -13,8 +12,7 @@
                 (assoc-in node [:children c] (insert-char next-node (rest chars))))))]
     (insert-char trie word)))
 
-(defn remove-word
-  [trie word]
+(defn remove-word [trie word]
   (letfn [(remove-char [node chars]
             (if (empty? chars)
               (if (:is-end node)
@@ -47,5 +45,32 @@
               :else (recur (get-in node [:children (first chars)]) (rest chars))))]
     (search-prefix trie prefix)))
 
-(def trie (create-node))
+(defn filter-trie [trie pred]
+  (letfn [(filter-node [node prefix]
+            ;; Проверяем текущий узел
+            (let [filtered-children
+                  (into {} (for [[char child] (:children node)
+                                 :let [filtered-child (filter-node child (str prefix char))]
+                                 :when (or (:is-end filtered-child)
+                                           (not (empty? (:children filtered-child))))]
+                             [char filtered-child]))]
+              ;; Возвращаем обновленный узел, если он содержит соответствующее слово
+              (if (and (:is-end node) (pred prefix))
+                ;; Сохраняем узел, если слово удовлетворяет предикату
+                {:is-end true :children filtered-children}
+                ;; Если узел не является концом нужного слова, просто возвращаем его детей
+                {:is-end false :children filtered-children})))]
+    (filter-node trie "")))
+
+(defn build-trie  [words]
+  (reduce insert-word (create-node) words))
+
+(defn trie-to-words [trie]
+  (letfn [(collect-words [node prefix]
+            (let [current-word (if (:is-end node) [prefix] [])
+                  children-words (mapcat (fn [[char child]]
+                                           (collect-words child (str prefix char)))
+                                         (:children node))]
+              (concat current-word children-words)))]
+    (collect-words trie "")))
 
