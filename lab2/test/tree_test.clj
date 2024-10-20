2beb7b7cc8f545461b9ed9e6b6e5f4f3f3f9d1b6
h1l3xx <alksmlkv@gmail.com>
2024-10-20 21:45:30 +0300

    feat(lab2): rewrite tests on generators

--- lab2/test/tree_test.clj
+++ lab2/test/tree_test.clj
@@ -1,116 +1,51 @@
 (ns tree-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
             [prefix-tree :refer :all])
   (:import (prefix_tree PrefixTreeDictionary)))
 
-(deftest test-insert-and-lookup
-  ;; Тестирование добавления и поиска ключей
-  (let [tree (-> (PrefixTreeDictionary. (create-node))
-                 (insert "cat")
-                 (insert "car")
-                 (insert "dog"))]
+(def key-gen
+  (gen/vector (gen/one-of [gen/int gen/string-alphanumeric]) 1 5))
 
-    ;; Проверка наличия добавленных ключей
-    (is (true? (lookup tree "car")))
-    (is (true? (lookup tree "car")))
-    (is (true? (lookup tree "dog")))
+(defspec test-insert-and-lookup 100
+  (prop/for-all [keys (gen/vector key-gen 3 10)]
+    (let [tree (reduce insert (PrefixTreeDictionary. (create-node)) keys)]
+      (every? #(lookup tree %) keys))))
 
-    ;; Проверка отсутствия несуществующих ключей
-    (is (false? (lookup tree "cow")))
-    (is (false? (lookup tree "d")))
-    (is (false? (lookup tree "do")))))
+(defspec test-delete-existing-key 100
+  (prop/for-all [keys (gen/vector key-gen 3 10)]
+    (let [tree (reduce insert (PrefixTreeDictionary. (create-node)) keys)
+          key-to-delete (first keys)
+          tree-after-delete (delete tree key-to-delete)]
+      (and (not (lookup tree-after-delete key-to-delete))
+           (every? #(lookup tree-after-delete %) (rest keys))))))
 
-    ;; Крайние случаи: пустая строка
-    (let [empty-tree (PrefixTreeDictionary. (create-node))]
-      (is (false? (lookup empty-tree "")))
-      (is (true? (lookup (insert empty-tree "") ""))))
+(defspec test-delete-nonexistent-key 100
+  (prop/for-all [keys (gen/vector key-gen 3 10)
+                 non-existent-key key-gen]
+    (let [tree (reduce insert (PrefixTreeDictionary. (create-node)) keys)
+          tree-after-delete (delete tree non-existent-key)]
+      (and (every? #(lookup tree-after-delete %) keys)
+           (not (lookup tree-after-delete non-existent-key))))))
 
-(deftest test-delete-existing-key
-  ;; Тестирование удаления существующего ключа
-  (let [tree (-> (PrefixTreeDictionary. (create-node))
-                 (insert "cat")
-                 (insert "car")
-                 (insert "dog"))
-        tree-after-delete (delete tree "car")]
+(defspec test-trie-keys 100
+  (prop/for-all [keys (gen/vector key-gen 3 10)]
+    (let [tree (reduce insert (PrefixTreeDictionary. (create-node)) keys)]
+      (= (set (trie-keys tree)) (set keys)))))
 
-    (is (false? (lookup tree-after-delete "car")))
-    (is (true? (lookup tree-after-delete "cat")))
-    (is (true? (lookup tree-after-delete "dog")))))
+(defspec test-merge-with-tree 100
+  (prop/for-all [keys1 (gen/vector key-gen 3 10)
+                 keys2 (gen/vector key-gen 3 10)]
+    (let [tree1 (reduce insert (PrefixTreeDictionary. (create-node)) keys1)
+          tree2 (reduce insert (PrefixTreeDictionary. (create-node)) keys2)
+          merged-tree (merge-tries tree1 tree2)]
+      (= (set (trie-keys merged-tree)) (set (concat keys1 keys2))))))
 
-(deftest test-delete-nonexistent-key
-  ;; Тестирование удаления несуществующего ключа
-  (let [tree (-> (PrefixTreeDictionary. (create-node))
-                 (insert "cat")
-                 (insert "car")
-                 (insert "dog"))
-        tree-after-delete (delete tree "cow")]
-
-    (is (true? (lookup tree-after-delete "cat")))
-    (is (true? (lookup tree-after-delete "car")))
-    (is (true? (lookup tree-after-delete "dog")))))
-
-(deftest test-delete-empty-string
-  ;; Тестирование удаления пустой строки
-  (let [tree (-> (PrefixTreeDictionary. (create-node))
-                 (insert "cat")
-                 (insert "car")
-                 (insert "dog"))
-        tree-with-empty-key (insert tree "")
-        tree-after-delete-empty (delete tree-with-empty-key "")]
-
-    (is (true? (lookup tree-with-empty-key "")))
-    (is (false? (lookup tree-after-delete-empty "")))))
-
-(deftest test-delete-empty-tree
-  ;; Тестирование удаления из пустого дерева
-  (let [empty-tree (PrefixTreeDictionary. (create-node))
-        tree-after-delete (delete empty-tree "nonexistent-key")]
-
-    (is (false? (lookup tree-after-delete "nonexistent-key"))) ;; должно быть nil
-    (is (= (trie-keys tree-after-delete) ())))) ;; должно быть пустое дерево
-
-(deftest test-trie-keys
-  (testing "Получение всех ключей из дерева"
-    (let [tree (-> (PrefixTreeDictionary. (create-node))
-                   (insert [1 2 3])
-                   (insert [4 5 6])
-                   (insert [7 8 9])
-                   (insert ["a" "b" "c"])
-                   (insert [1 2 4]))]
-      ;; Получение ключей
-      (let [keys (trie-keys tree)]
-        ;; Проверка ожидаемых ключей
-        (is (= keys
-               [[1 2 3] [1 2 4] [4 5 6] [7 8 9] ["a" "b" "c"]]))))))
-
-(deftest test-merge-with-tree
-  ;; Тестирование слияния деревьев
-  (let [tree1 (-> (PrefixTreeDictionary. (create-node))
-                  (insert "cat")
-                  (insert "car"))
-        tree2 (-> (PrefixTreeDictionary. (create-node))
-                  (insert "car")
-                  (insert "dog"))
-        merged-tree (merge-tries tree1 tree2)]
-
-    (println merged-tree)
-    ;; Проверяем наличие ключей после слияния
-    (is (true? (lookup merged-tree "cat")))
-    (is (true? (lookup merged-tree "car")))
-    (is (true? (lookup merged-tree "dog")))
-
-    ;; Проверяем, что ключи из первого и второго дерева присутствуют
-    (is (= (trie-keys merged-tree)) '("cat" "car" "dog"))
-
-    ;; Проверяем слияние с пустым деревом
+(defspec test-delete-empty-tree -100
+  (prop/for-all [key-to-delete key-gen]
     (let [empty-tree (PrefixTreeDictionary. (create-node))
-          merged-with-empty (merge-tries tree1 empty-tree)]
-      (is (= (trie-keys merged-with-empty)) '("cat" "car")))))
-
-(deftest test-merge-with-tree-no-keys
-  ;; Проверяем слияние пустых деревьев
-  (let [empty-tree1 (PrefixTreeDictionary. (create-node))
-        empty-tree2 (PrefixTreeDictionary. (create-node))
-        merged-tree (merge-tries empty-tree1 empty-tree2)]
-    (is (= (trie-keys merged-tree) []))))
-
+          tree-after-delete (delete empty-tree key-to-delete)]
+      (empty? (trie-keys tree-after-delete)))))
