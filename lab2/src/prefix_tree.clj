3c8cbcdd0d0442649307982567c1b30b02f56a55
h1l3xx <alksmlkv@gmail.com>
2024-10-20 20:39:05 +0300

    feat(lab2): refactor and add unit tests

--- lab2/src/prefix_tree.clj
+++ lab2/src/prefix_tree.clj
@@ -8,51 +8,57 @@
   (lookup [this key] "Проверить, существует ли ключ в словаре")
   (delete [this key] "Удалить ключ из словаря")
   (trie-keys [this] "Вернуть все ключи")
-  (entries [this] "Вернуть все пары ключ-флаг"))
+  (entries [this] "Вернуть все пары ключ-флаг")
+  (merge-tries [this other] "Объединить два префиксных дерева"))
 
 (deftype PrefixTreeDictionary [root]
   IDictionary
   ;; Вставка ключа в префиксное дерево
   (insert [this key]
-    (letfn [(insert-seq [node chars]
-              (if (empty? chars)
-                (assoc node :is-end true)  ;; Устанавливаем флаг конца слова
-                (let [c (first chars)
-                      next-node (get-in node [:children c] (create-node))]
-                  (assoc-in node [:children c] (insert-seq next-node (rest chars))))))]
+    (letfn [(insert-seq [node elems]
+              (if (empty? elems)
+                (assoc node :is-end true)  ;; Устанавливаем флаг конца ключа
+                (let [elem (first elems)
+                      next-node (get-in node [:children elem] (create-node))]
+                  (assoc-in node [:children elem] (insert-seq next-node (rest elems))))))]
       (PrefixTreeDictionary. (insert-seq root key))))
 
-  ;; Проверка наличия ключа в префиксном дереве
+  ;; Проверка наличия ключа в префиксное дерево
   (lookup [this key]
-    (letfn [(lookup-seq [node chars]
-              (if (empty? chars)
-                (:is-end node)  ;; Возвращаем флаг окончания слова
-                (let [next-node (get-in node [:children (first chars)])]
-                  (when next-node
-                    (lookup-seq next-node (rest chars))))))]
+    (letfn [(lookup-seq [node elems]
+              (if (empty? elems)
+                (:is-end node)  ;; Возвращаем флаг окончания ключа
+                (let [next-node (get-in node [:children (first elems)])]
+                  (if next-node
+                    (lookup-seq next-node (rest elems))
+                    false))))]  ;; Если узел не найден, возвращаем false
       (lookup-seq root key)))
 
-  ;; Удаление ключа из префиксного дерева
+  ;; Удаление ключа из дерева
   (delete [this key]
-    (letfn [(remove-seq [node chars]
-              (if (empty? chars)
-                (assoc node :is-end false)  ;; Убираем флаг конца слова
-                (let [c (first chars)
-                      next-node (get-in node [:children c])]
-                  (when next-node
-                    (let [updated-node (remove-seq next-node (rest chars))]
-                      (if (and (not (:is-end updated-node))
-                               (empty? (:children updated-node)))
-                        (update node :children dissoc c)  ;; Удаляем узел, если он пуст
-                        (assoc-in node [:children c] updated-node)))))))]
-      (PrefixTreeDictionary. (remove-seq root key))))
+    (if (empty? (:children root))
+      ;; Если дерево пустое, возвращаем его без изменений
+      this
+      (letfn [(remove-seq [node elems]
+                (if (empty? elems)
+                  (assoc node :is-end false)  ;; Убираем флаг конца ключа
+                  (let [elem (first elems)
+                        next-node (get-in node [:children elem])]
+                    (if next-node
+                      (let [updated-node (remove-seq next-node (rest elems))]
+                        (if (and (not (:is-end updated-node))
+                                 (empty? (:children updated-node)))
+                          (update node :children dissoc elem)  ;; Удаляем узел, если он пуст
+                          (assoc-in node [:children elem] updated-node)))
+                      node))))]
+        (PrefixTreeDictionary. (remove-seq root key)))))
 
   ;; Получение всех ключей
   (trie-keys [this]
     (letfn [(collect-keys [node prefix]
-              (let [current-keys (if (:is-end node) [prefix] [])
-                    children-keys (mapcat (fn [[char child]]
-                                            (collect-keys child (conj prefix char)))
+              (let [current-keys (if (:is-end node) [prefix] [])  ;; Ключи хранятся в виде коллекций
+                    children-keys (mapcat (fn [[elem child]]
+                                            (collect-keys child (conj prefix elem)))
                                           (:children node))]
                 (concat current-keys children-keys)))]
       (collect-keys root [])))
@@ -60,9 +66,20 @@
   ;; Получение всех пар ключ-флаг
   (entries [this]
     (letfn [(collect-entries [node prefix]
-              (let [current-entry (if (:is-end node) [[prefix true]] [])
-                    children-entries (mapcat (fn [[char child]]
-                                               (collect-entries child (conj prefix char)))
-                                             (:children node))]
-                (concat current-entry children-entries)))]
-      (collect-entries root []))))
+              (if (nil? node)
+                [] ;; Если узел nil, возвращаем пустой список
+                (let [current-entry (if (:is-end node) [[prefix true]] [])  ;; Ключи в виде коллекций
+                      children-entries (mapcat (fn [[elem child]]
+                                                 (collect-entries child (conj prefix elem)))
+                                               (:children node))]
+                  (concat current-entry children-entries))))]
+      (collect-entries root [])))
+
+  ;; Функция для объединения двух деревьев через вставку ключей
+  (merge-tries [this other]
+    (let [other-keys (trie-keys other)]
+      (reduce (fn [tree key]
+                (insert tree key))
+              this
+              other-keys))))
+
--- lab2/test/tree_test.clj
+++ lab2/test/tree_test.clj
@@ -1,2 +1,116 @@
 (ns tree-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [prefix-tree :refer :all])
+  (:import (prefix_tree PrefixTreeDictionary)))
+
+(deftest test-insert-and-lookup
+  ;; Тестирование добавления и поиска ключей
+  (let [tree (-> (PrefixTreeDictionary. (create-node))
+                 (insert "cat")
+                 (insert "car")
+                 (insert "dog"))]
+
+    ;; Проверка наличия добавленных ключей
+    (is (true? (lookup tree "car")))
+    (is (true? (lookup tree "car")))
+    (is (true? (lookup tree "dog")))
+
+    ;; Проверка отсутствия несуществующих ключей
+    (is (false? (lookup tree "cow")))
+    (is (false? (lookup tree "d")))
+    (is (false? (lookup tree "do")))))
+
+    ;; Крайние случаи: пустая строка
+    (let [empty-tree (PrefixTreeDictionary. (create-node))]
+      (is (false? (lookup empty-tree "")))
+      (is (true? (lookup (insert empty-tree "") ""))))
+
+(deftest test-delete-existing-key
+  ;; Тестирование удаления существующего ключа
+  (let [tree (-> (PrefixTreeDictionary. (create-node))
+                 (insert "cat")
+                 (insert "car")
+                 (insert "dog"))
+        tree-after-delete (delete tree "car")]
+
+    (is (false? (lookup tree-after-delete "car")))
+    (is (true? (lookup tree-after-delete "cat")))
+    (is (true? (lookup tree-after-delete "dog")))))
+
+(deftest test-delete-nonexistent-key
+  ;; Тестирование удаления несуществующего ключа
+  (let [tree (-> (PrefixTreeDictionary. (create-node))
+                 (insert "cat")
+                 (insert "car")
+                 (insert "dog"))
+        tree-after-delete (delete tree "cow")]
+
+    (is (true? (lookup tree-after-delete "cat")))
+    (is (true? (lookup tree-after-delete "car")))
+    (is (true? (lookup tree-after-delete "dog")))))
+
+(deftest test-delete-empty-string
+  ;; Тестирование удаления пустой строки
+  (let [tree (-> (PrefixTreeDictionary. (create-node))
+                 (insert "cat")
+                 (insert "car")
+                 (insert "dog"))
+        tree-with-empty-key (insert tree "")
+        tree-after-delete-empty (delete tree-with-empty-key "")]
+
+    (is (true? (lookup tree-with-empty-key "")))
+    (is (false? (lookup tree-after-delete-empty "")))))
+
+(deftest test-delete-empty-tree
+  ;; Тестирование удаления из пустого дерева
+  (let [empty-tree (PrefixTreeDictionary. (create-node))
+        tree-after-delete (delete empty-tree "nonexistent-key")]
+
+    (is (false? (lookup tree-after-delete "nonexistent-key"))) ;; должно быть nil
+    (is (= (trie-keys tree-after-delete) ())))) ;; должно быть пустое дерево
+
+(deftest test-trie-keys
+  (testing "Получение всех ключей из дерева"
+    (let [tree (-> (PrefixTreeDictionary. (create-node))
+                   (insert [1 2 3])
+                   (insert [4 5 6])
+                   (insert [7 8 9])
+                   (insert ["a" "b" "c"])
+                   (insert [1 2 4]))]
+      ;; Получение ключей
+      (let [keys (trie-keys tree)]
+        ;; Проверка ожидаемых ключей
+        (is (= keys
+               [[1 2 3] [1 2 4] [4 5 6] [7 8 9] ["a" "b" "c"]]))))))
+
+(deftest test-merge-with-tree
+  ;; Тестирование слияния деревьев
+  (let [tree1 (-> (PrefixTreeDictionary. (create-node))
+                  (insert "cat")
+                  (insert "car"))
+        tree2 (-> (PrefixTreeDictionary. (create-node))
+                  (insert "car")
+                  (insert "dog"))
+        merged-tree (merge-tries tree1 tree2)]
+
+    (println merged-tree)
+    ;; Проверяем наличие ключей после слияния
+    (is (true? (lookup merged-tree "cat")))
+    (is (true? (lookup merged-tree "car")))
+    (is (true? (lookup merged-tree "dog")))
+
+    ;; Проверяем, что ключи из первого и второго дерева присутствуют
+    (is (= (trie-keys merged-tree)) '("cat" "car" "dog"))
+
+    ;; Проверяем слияние с пустым деревом
+    (let [empty-tree (PrefixTreeDictionary. (create-node))
+          merged-with-empty (merge-tries tree1 empty-tree)]
+      (is (= (trie-keys merged-with-empty)) '("cat" "car")))))
+
+(deftest test-merge-with-tree-no-keys
+  ;; Проверяем слияние пустых деревьев
+  (let [empty-tree1 (PrefixTreeDictionary. (create-node))
+        empty-tree2 (PrefixTreeDictionary. (create-node))
+        merged-tree (merge-tries empty-tree1 empty-tree2)]
+    (is (= (trie-keys merged-tree) []))))
+
